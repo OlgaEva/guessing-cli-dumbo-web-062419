cba06b199aa2365bc47b2247d127a939db41baec
Olga Rosas <olga.rosas@gmail.com>
2021-01-06 20:46:18 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,7 @@
 require 'pry'
 
 def run_guessing_game
+<<<<<<< HEAD
   users_input = ""
   random_number = rand(1..6).to_s
 
@@ -16,6 +17,18 @@ def run_guessing_game
         break
     else
         puts "The computer guessed #{random_number}."
+=======
+  users_guess = nil
+  puts "Guess a number between 1 and 6:"
+  users_guess = gets.chomp
+  while users_guess != "exit"
+    random_num = 1 + rand(6)
+    if users_guess == random_num
+        puts "You guessed the correct number!" # random numbers in Ruby are generated thusly: 1 + rand(6)
+    elsif users_guess != random_num
+        puts "The computer guessed #{random_num}."
+
+>>>>>>> ff7a6a1e735ab79d2f9fe84564eb5b08a7fe214c
       end
     end
 end
